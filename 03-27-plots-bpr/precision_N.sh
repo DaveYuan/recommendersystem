67b64b57ab60e803f7924b2655762fec46f31a5f
Shivani <shivani.mah.91@gmail.com>
2013-03-28 15:20:21 +0100

    BPR

--- 03-27-plots-bpr/precision_N.sh
+++ 03-27-plots-bpr/precision_N.sh
@@ -4,6 +4,6 @@ set xlabel "N"
 set ylabel "Precision(N)"
 
 set title "Precision Plot. ML-100K"
-set ytics 0,0.010
+set ytics 0,0.0010
 
 plot "precision_per_n.txt" u 1:2 title 'Precision' w points 
